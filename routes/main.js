fd523d71bcaec2cc23bcdb0c2a879c3d82e28d51
Shim.hw <shim.hw0810@gmail.com>
2017-04-12 13:38:22 +0900

    Add comment

--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,10 @@
 module.exports = function(app, pool)
 {
 
-  
+
   var context = "http://localhost:3000/";
 
+  // [Util Method Define]
   function isURL(query) {
     var reg = new RegExp(/^(((http(s?))\:\/\/)?)([0-9a-zA-Z\-]+\.)+[a-zA-Z]{2,6}(\:[0-9]+)?(\/\S*)?$/);
     return reg.test(query);
@@ -14,15 +15,18 @@ module.exports = function(app, pool)
     return reg.test(query);
   }
 
+  // [User Case 1 : registers URL]
   app.post('/resister.json', function(req, res) {
     var long_url = req.query.url;
 
     if (long_url == undefined || !isURL(long_url)) {
+      // [Wrong URL]
       res.status(400).json({reason: "Not enough URL"});
     } else {
 
       var param = [long_url];
 
+      // [Connecting by Conection pool]
       pool.getConnection(function(err, conn) {
         conn.query("SELECT id FROM urls_tb WHERE long_url=?", param, function(err, rows) {
           if (err) {
@@ -32,18 +36,22 @@ module.exports = function(app, pool)
           }
 
             if (rows.length > 0) {
+              // [URL is already registered]
               res.status(200).json({url: context + rows[0].id});
             } else {
+              // [URL not exist : Generates record]
               conn.query("INSERT INTO urls_tb (long_url) VALUES (?)", param, function(err, rows) {
                 if (err) {
                   console.err(err);
                   conn.release();
                   throw err;
                 }
+                // [Return generated URL]
                 res.status(201).json({url: context + rows.insertId});
 
               });
             }
+            // [Connection Release]
             conn.release();
         });
       });
@@ -51,13 +59,16 @@ module.exports = function(app, pool)
 
   });
 
+  // [User Case 2 : accesses URL that retrieved from register response]
   app.get('/:id', function(req, res) {
 
     var param = [req.params.id];
 
     if (!isDigit(req.params.id)) {
+      // [Wrong ID : not digit]
       res.status(400).json({reason: "Wrong Id"});
     } else {
+      // [Connecting by Conection pool]
       pool.getConnection(function(err, conn) {
         conn.query("SELECT long_url FROM urls_tb WHERE id=?", param, function(err, rows) {
           if (err) {
@@ -67,8 +78,9 @@ module.exports = function(app, pool)
           }
 
           if (rows.length > 0) {
+            // [Redirecting Original URL]
             res.redirect(301, rows[0].long_url);
-
+            // [Increases number of visit for the URL record]
             conn.query("UPDATE urls_tb SET visits_cnt = visits_cnt+1 WHERE id=?", param, function(err) {
               if (err) {
                 console.err(err);
@@ -77,8 +89,10 @@ module.exports = function(app, pool)
               }
             });
           } else {
+            // [ID not exist]
             res.status(400).json({reason: "Wrong Id"});
           }
+          // [Connection Release]
           conn.release();
         });
       });
@@ -90,8 +104,10 @@ module.exports = function(app, pool)
     var param = [req.params.id];
 
     if (!isDigit(req.params.id)) {
+      // [Wrong ID : not digit]
       res.status(400).json({reason: "Wrong Id"});
     } else {
+      // [Connecting by Conection pool]
       pool.getConnection(function(err, conn) {
         conn.query("SELECT visits_cnt FROM urls_tb WHERE id=?", param, function(err, rows) {
           if (err) {
@@ -99,11 +115,14 @@ module.exports = function(app, pool)
             conn.release();
             throw err;
           }
+          // [Return number of visit for the URL record]
           if (rows.length > 0) {
             res.status(200).json({visits: rows[0].visits_cnt});
           } else {
+            // [ID not exist]
             res.status(400).json({reason: "Wrong Id"});
           }
+          // [Connection Release]
           conn.release();
         });
       });
